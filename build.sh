968e4367b788d6ae78b1b59e2f9ba823ef239d0a
Francesco Bianco <info.francescobianco@gmail.com>
2022-04-02 13:07:41 +0200

    First commit

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm -fr .github/workflows/*.yml
 
 tags="$(git ls-remote --tags --sort="v:refname" https://github.com/bpkg/bpkg.git | cut -d/ -f3 | tail -n2)"
 
-while IFS= read line || [[ -n "${line}" ]]; do
+while IFS= read -r line || [[ -n "${line}" ]]; do
   [[ -z "${line}" ]] && continue
   [[ "${line::1}" == "#" ]] && continue
 
@@ -13,7 +13,10 @@ while IFS= read line || [[ -n "${line}" ]]; do
   title=$(echo $line | cut -d'#' -f2 | xargs)
   platform=$(echo $line | cut -d'#' -f3 | xargs)
   image=$(echo $line | cut -d'#' -f4 | xargs)
-  bootstrap=$(echo $line | cut -d'#' -f5 | xargs)
+  bootstrap=$(echo "$line" | cut -d'#' -f5 | xargs)
+
+
+  echo "$bootstrap"
 
   sed -e 's!%name%!'"${name}"'!' \
       -e 's!%title%!'"${title}"'!' \
